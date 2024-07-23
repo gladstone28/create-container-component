a5153074f61f982c94f36e81da0c020613b85c67
Gladstone Rose <gladstonerose@hotmail.com>
2024-07-23 18:06:21 -0500

    changed to project files

--- src/backup-index.js
+++ src/backup-index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
-import GuineaPigs from './GuineaPigs';
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( <
-    React.StrictMode >
-    <
-    GuineaPigs / >
-    <
-    /React.StrictMode>
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
